13366b66a84886d506cc3186aa1a41b5b513fd10
amit-singh-mobcoder <amit.s@mobcoder.com>
2024-08-23 15:37:01 +0530

    debug and fix error

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,30 +2,45 @@ import express, {Request, Response, NextFunction} from 'express';
 import { ApiError } from '../utils/ApiError';
 import { JwtWrapper } from "../wrappers/jwt.wrapper";
 import { ConstantHelper } from '../constants';
-import { UserModel } from '../models/user.model';
+import { UserModel, IUser } from '../models/user.model';
 
-const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
+declare module 'express-serve-static-core' {
+    interface Request {
+        user?: IUser | null;  // Add optional user property to Request interface
+    }
+}
 
+const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    
-        if (!token){
-            throw new ApiError(401, "Unauthorized request")
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request");
         }
-        
+
         const secretKey = ConstantHelper.jwtSecretKey;
         const decodedToken = JwtWrapper.verify(token, secretKey);
-    
-        const user = UserModel.findById(decodedToken?.id)
-    
-        if (!user) {
-            throw new ApiError(401, "Invalid Access Token")
+
+        // Ensure decodedToken is an object with an id property
+        if (typeof decodedToken !== 'object' || !decodedToken || !('id' in decodedToken)) {
+            throw new ApiError(401, "Invalid Access Token");
         }
-    
+
+        // Access the id property safely
+        const user = await UserModel.findById((decodedToken as { id: string }).id).select("-password");
+
+        if (!user) {
+            throw new ApiError(401, "Invalid Access Token");
+        }
+
+        // Attach the user to the request object
         req.user = user;
         next();
-
     } catch (error) {
+        next(error);
     }
-
 }
+
+
+
+export { verifyJWT }
